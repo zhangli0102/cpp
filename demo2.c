6251c55b230929ef746f7030cf74fb40f37dfebf
Li Zhang <zhangli@gwu.edu>
2016-07-13 21:16:44 -0400

    Init Commit

---
+++ demo2.c
@@ -0,0 +1,5 @@
+#include "iostream"
+int main(void) {
+  std::cout << "Hello! This is a C++ from Li Zhang!\n";
+  return 0;
+}
---
+++ demo3.c
@@ -0,0 +1,10 @@
+#include "iostream"
+int main(void) {
+  std :: cout << "My name is Schwein!\n";
+  return 0;
+}
+
+
+
+
+
