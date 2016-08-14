4086a165ad4e92281c72a674fa3ccab9749066bb
Li Zhang <zhangli@gwu.edu>
2016-08-14 17:45:54 -0400

    New

---
+++ demo4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  std::cout << "Enter two numbers:";
+  int v1 = 0, v2 = 0;
+  cin >> v1 >> v2;
+  cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << "\n";
+  return 0;
+}
+
